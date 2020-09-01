10b1f49b04ed3b17f8253f307a9d267879f4da1c
Atankatum <atakan.senturk@elev.ga.ntig.se>
2020-09-01 10:52:38 +0200

    initial commit

---
+++ Hangman/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            // fnutt File.WriteAllText("words.txt", "axel haha");
+            
+            string aksleliljaful = File.ReadAllText("words.txt");
+            
+            Console.WriteLine(aksleliljaful);
+            
+            // Console.ReadLine();
+
+            // string[] words = {"Hat", "Coat", "Pants"};
+            // File.WriteAllLines("words.txt", words);
+
+
+            if (File.Exists("words.txt"))
+            {
+
+            string [] words = File.ReadAllLines("words.txt");
+            
+            Console.WriteLine(words[1]);
+            
+            Console.ReadLine();
+            }
+        }
+    }
+}
